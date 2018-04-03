02e8ee4b82405cb28a01d6fe9d222f9a9d01fb4d
karol <cavilani@gmail.com>
2018-04-03 21:37:25 +0100

    Creating some test examples to try the new asserts methods I'm going to create

---
+++ tests/budTaskTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class BudTaskTest extends WebTestCase
+{
+	
+	protected function setUp()
+    {
+    	//Example data
+        $transactionData = [
+    		'id' 				=> '153543', 
+    		'totalAmount' 		=> '21.5',
+    		'currency' 			=> 'GBP',
+    		'transactionDate' 	=> '06/03/2018 11:15'
+    		];
+
+    	$this->transactionArray  = $transactionData;
+    	$this->transactionObject = (object) $transactionData;
+    	$this->transactionJson   = json_encode($transactionData);
+    }
+
+    // These are no real unit tests, they are examples to try the new asserts methods
+
+    public function testExampleShouldPass()
+    {
+    	//Array 	
+    
+    	$this->assertPropertyExist('id', $this->transactionArray);    
+   
+    	$this->assertContainsValue('GBP', $this->transactionArray);
+    	
+
+    	//Object
+
+    	$this->assertPropertyExist('id', $this->transactionObject);    	
+
+    	$this->assertContainsValue('GBP', $this->transactionObject);
+
+
+    	//Json String
+    	$this->assertPropertyExist('id', $this->transactionJson);
+
+    	$this->assertContainsValue('GBP', $this->transactionJson);
+ 	
+    }
+
+   
+    public function  testExampleShouldFail()
+    {
+    	
+    	//Array        
+    	$this->assertPropertyExist('description', $this->transactionArray);
+    
+    	$this->assertContainsValue('EUR', $this->transactionArray);
+
+
+    	//Object
+    	$this->assertPropertyExist('description', $this->transactionObject);
+    	
+    	$this->assertContainsValue('EUR', $this->transactionObject);
+
+
+    	//Json string
+    	$this->assertPropertyExist('description', $this->transactionJson);
+
+    	$this->assertContainsValue('EUR', $this->transactionJson);  
+    	
+    }
+    
+
+}
